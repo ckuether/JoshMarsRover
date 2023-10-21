c8d1712a75584a15f8cef38970f7eff8e552300e
Corey Kuether <coreykuether1@gmail.com>
2023-10-21 12:24:18 -0600

    Integrated Parcelize & removed redundant code

--- app/src/main/java/com/example/joshmarsrover/data/model/Camera.kt
+++ app/src/main/java/com/example/joshmarsrover/data/model/Camera.kt
@@ -1,34 +1,10 @@
 package com.example.joshmarsrover.data.model
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Camera(
     val full_name: String,
     val name: String
-): Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString()!!,
-        parcel.readString()!!
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(full_name)
-        parcel.writeString(name)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Camera> {
-        override fun createFromParcel(parcel: Parcel): Camera {
-            return Camera(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Camera?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+): Parcelable
--- app/src/main/java/com/example/joshmarsrover/data/model/Rover.kt
+++ app/src/main/java/com/example/joshmarsrover/data/model/Rover.kt
@@ -1,12 +1,13 @@
 package com.example.joshmarsrover.data.model
 
-import android.os.Parcel
 import android.os.Parcelable
 import com.example.joshmarsrover.common.DateFormat
 import com.example.joshmarsrover.common.toDate
 import com.example.joshmarsrover.common.toFormattedString
+import kotlinx.parcelize.Parcelize
 import java.util.Date
 
+@Parcelize
 data class Rover(
     val cameras: List<Camera>,
     val id: Int,
@@ -18,7 +19,7 @@ data class Rover(
     val status: String,
     val total_photos: Int,
     var photos: List<Photo>?
-): Parcelable{
+): Parcelable {
     val maxDate: Date?
         get() = max_date.toDate(DateFormat.NETWORK_FORMAT)
 
@@ -54,46 +55,4 @@ data class Rover(
 
     val landingDateDescription: String
         get() = "Landing: $formattedLandingDate"
-
-    constructor(parcel: Parcel) : this(
-        parcel.createTypedArrayList(Camera)!!,
-        parcel.readInt(),
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readInt(),
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readInt(),
-        parcel.createTypedArrayList(Photo.CREATOR)
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeTypedList(cameras)
-        parcel.writeInt(id)
-        parcel.writeString(landing_date)
-        parcel.writeString(launch_date)
-        parcel.writeString(max_date)
-        parcel.writeInt(max_sol)
-        parcel.writeString(name)
-        parcel.writeString(status)
-        parcel.writeInt(total_photos)
-        parcel.writeTypedList(photos)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Rover> {
-        override fun createFromParcel(parcel: Parcel): Rover {
-            return Rover(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Rover?> {
-            return arrayOfNulls(size)
-        }
-    }
-
 }
